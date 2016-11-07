b587773f4acd3abadbd3dfcf19745ce078718512
dougwells <dougwells@gmail.com>
2016-11-07 07:22:12 -0700

    iOS 10. Simple Coffee App

---
+++ CoffeeApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CoffeeApp
+//
+//  Created by Doug Wells on 11/6/16.
+//  Copyright © 2016 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numCups: UITextField!
+    
+    @IBOutlet weak var totCost: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        numCups.layer.cornerRadius = 15.0
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func calcPressed(_ sender: Any) {
+    }
+
+}
+
